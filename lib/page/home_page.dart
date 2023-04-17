c400a86e54998966cbc8d14cff80b648d5732e90
Luk Gutierrez <lucianogutierrezagustin@gmail.com>
2023-04-17 16:31:59 -0300

    version_edit

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,49 +13,55 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    final iconList = <IconData>[Icons.star, Icons.star, Icons.star, Icons.abc];
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.white,
-        child: Center(
-          child: Text(
-            "Iniciar",
-            style: TextStyle(color: Colors.black, fontSize: 10),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.white,
+          child: Center(
+            child: Text(
+              "Iniciar",
+              style: TextStyle(color: Colors.black, fontSize: 10),
+            ),
           ),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => PageOne()),
+            );
+          },
         ),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => PageOne()),
-          );
-        },
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: AnimatedBottomNavigationBar(
-        icons: iconList,
-        activeColor: Colors.white,
-        inactiveColor: Colors.white,
-        activeIndex: 0,
-        gapLocation: GapLocation.center,
-        notchSmoothness: NotchSmoothness.verySmoothEdge,
-        onTap: (b) {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => Pagetwo()),
-          );
-        },
-      ),
-      backgroundColor: Colors.black,
-      body: Stack(
-        children: [
+        backgroundColor: Colors.black,
+        body: Stack(children: [
           /////IMAGE-BACKGROUND//////
 
           Container(
             child: Image(image: AssetImage("assets/fondo.gif")),
           ),
-        ],
-      ),
-    );
+          Row(
+            children: [
+              SpeedDial(
+                backgroundColor: Color(0xFFF00A884),
+                animatedIcon: AnimatedIcons.menu_close,
+                visible: true,
+                curve: Curves.bounceInOut,
+                children: [
+                  SpeedDialChild(
+                      child: Icon(Icons.photo),
+                      backgroundColor: Color(0xFFF00A884),
+                      onTap: () {}),
+                  SpeedDialChild(
+                      child: Icon(Icons.block),
+                      backgroundColor: Color(0xFFF00A884),
+                      onTap: () {}),
+                  SpeedDialChild(
+                      child: Icon(Icons.camera_front),
+                      backgroundColor: Color(0xFFF00A884),
+                      labelStyle: TextStyle(fontSize: 20.0),
+                      onTap: () {}),
+                ],
+              ),
+            ],
+          ),
+        ]));
   }
 }
 
